38c7192ef60d010044410cfa1f83f84b14b61b0c
yen <84611800+hungyennn@users.noreply.github.com>
2023-03-15 01:11:35 +0800

    Add files via upload

---
+++ substitution.c
@@ -0,0 +1,78 @@
+#include <cs50.h>
+#include <stdio.h>
+#include <string.h>
+#include <ctype.h>
+
+int main(int argc, string argv[])
+{
+
+    // check one command-line argument
+    if (argc != 2)
+    {
+        printf("Usage: ./substitution key\n");
+        return 1;
+    }
+
+    // validate key only consists alphabets
+    string key = argv[1];
+
+    for(int i = 0; i < strlen(key); i++)
+    {
+        if(!isalpha(key[i]))
+        {
+            printf("Usage: ./substitution key\n");
+            return 1;
+        }
+    }
+
+    // validate key consists of 26 characters
+    if (strlen(key) != 26)
+    {
+        printf("Key must contain 26 characters.\n");
+        return 1;
+    }
+
+    // validate each key consists unique characters
+    for (int i = 0; i < strlen(key); i++)
+    {
+        for(int j = i + 1; j < strlen(key); j++)
+        {
+            if (toupper(key[i]) == toupper(key[j]))
+            {
+                printf("Usage: ./substitution key\n");
+                return 1;
+            }
+        }
+    }
+
+    string plaintext = get_string("plaintext: ");
+
+    for (int i = 0; i < strlen(key); i++)
+    {
+        if (islower(key[i]))
+        {
+            key[i] = key[i] - 32;
+        }
+    }
+
+    printf("ciphertext: ");
+
+    for (int i = 0; i < strlen(plaintext); i++)
+    {
+        if (isupper(plaintext[i]))
+        {
+           int letter = plaintext[i] - 65;
+           printf("%c", key[letter]);
+        }
+        else if (islower(plaintext[i]))
+        {
+            int letter = plaintext[i] - 97;
+            printf("%c", key[letter] + 32);
+        }
+        else
+        {
+            printf("%c", plaintext[i]);
+        }
+    }
+    printf("\n");
+}
